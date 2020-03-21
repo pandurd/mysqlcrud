65e1a4b81c828f6d3ab0e2207180624682d3cd18
Pandurangan D <Pandurangan.D@mrcooper.com>
2020-03-21 23:32:31 +0530

    Finished cru

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,54 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userService = require('../services/user.service');
 
-// routes
-router.post('/authenticate', authenticate);
-router.post('/register', register);
-router.get('/', getAll);
-router.get('/current', getCurrent);
-router.get('/:id', getById);
-router.put('/:id', update);
-router.delete('/:id', _delete);
-router.post('/image', uploadPic);
-
-module.exports = router;
-
-function authenticate(req, res, next) {
-    userService.authenticate(req.body)
-        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => next(err));
-}
-
 function register(req, res, next) {
-    userService.getById(req.user.sub)
-    .then(async function(user){
-            if(user.isadmin)
-                    userService.create(req.body, req)
-                        .then(() => res.json({}))
-                        .catch(err => next(err));
-            else
-                throw 'admin only.'
-        })
+    userService.create(req.body, req)
+        .then(() => res.json({}))
         .catch(err => next(err));
 }
 
 function getAll(req, res, next) {
-    userService.getById(req.user.sub)
-    .then(async function(user){
-        console.log(user);
-            if(user.isadmin)
-                userService.getAll()
-                    .then(users => res.json(users))
-                    .catch(err => next(err));
-            else
-                    throw 'admin only.'
-            })
-            .catch(err => next(err));
-}
-
-function getCurrent(req, res, next) {
-    userService.getById(req.user.sub)
-        .then(user => user ? res.json(user) : res.sendStatus(404))
+    userService.getAll()
+        .then(users => res.json(users))
         .catch(err => next(err));
 }
 
@@ -60,16 +21,10 @@ function getById(req, res, next) {
 }
 
 function update(req, res, next) {
-    userService.getById(req.user.sub)
-    .then(async function(user){
-            if(user.isadmin)
-                userService.update(req.params.id, req.body, req)
-                    .then(() => res.json({}))
-                    .catch(err => next(err));
-            else
-                throw 'admin only'
-    }).catch(err => next(err));
-}
+    userService.update(req.params.id, req.body, req)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+};
 
 function _delete(req, res, next) {
     userService.delete(req.params.id, req)
@@ -77,9 +32,53 @@ function _delete(req, res, next) {
         .catch(err => next(err));
 }
 
-function uploadPic(req, res, next){
-    userService.addImage(req.body.photo)
-    .then(() => res.json({}))
-    .catch(err => next(err));
-    
+async function create() {
+    const { Client } = require('pg')
+    const client = new Client({
+      user: "yqibwidetitjus",
+      password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+      database: "da4cq00t6pfvr5",
+      port: 5432,
+      host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+      ssl: true
+  }); 
+    await client.connect()
+    const res = await client.query('CREATE TABLE account(' +
+       'username VARCHAR (50) UNIQUE NOT NULL,' +
+       'password VARCHAR (50) NOT NULL,' +
+       'name VARCHAR (355) UNIQUE NOT NULL,' +
+       'isadmin BOOLEAN NOT NULL)'
+    );
+    await client.end()
+  }
+
+  async function insert() {
+    const { Client } = require('pg')
+    const client = new Client({
+      user: "yqibwidetitjus",
+      password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+      database: "da4cq00t6pfvr5",
+      port: 5432,
+      host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+      ssl: true
+  }); 
+    await client.connect()
+    const res = await client.query('INSERT INTO account(username, password, name, isadmin)'+
+    "VALUES ('test', 'test', 'Test', 'true')");
+    await client.end()
+  }
+  
+function createTable(){
+    create().then(function(){
+    })
 }
+  
+router.get('/createtable', createTable);
+router.post('/register', register);
+router.get('/', getAll);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,127 +1,92 @@
-const config = require('config.json');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const db = require('../helpers/db');
-const User = db.User;
-const Customer = db.Customer;
+const { Client } = require('pg')
 
 module.exports = {
-    authenticate,
     getAll,
-    getAllCustomer,
     getById,
     create,
     update,
     delete: _delete,
-    getAllCustomerWithDate,
-    addImage,
 };
 
-async function authenticate({ username, password }) {
-    const user = await User.findOne({ username });
-    console.log("found", user, username, password);
-    if (user && bcrypt.compareSync(password, user.hash)) {
-        const { hash, ...userWithoutHash } = user.toObject();
-        const token = jwt.sign({ sub: user.id, isadmin: user.isadmin }, config.secret);
-        return {
-            ...userWithoutHash,
-            token
-        };
-    }
-}
-
 async function getAll() {
-    return await User.find().select('-hash');
+    const client = new Client({
+        user: "yqibwidetitjus",
+        password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+        database: "da4cq00t6pfvr5",
+        port: 5432,
+        host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+        ssl: true
+      }); 
+    
+    await client.connect()
+    const res = await client.query('select * from account');
+    await client.end();
+    return res.rows;
 }
-
-async function getAllCustomer() {
-    return await Customer.find().select('-hash');
-}
-
- function getAllCustomerWithDate(startDate, endDate) {
-
-   return new Promise(function (resolve, reject){
-        Customer.find({
-            date: { 
-                $gte: new Date(startDate),
-                $lte: new Date(endDate),
-            }
-            }, null,function(err, docs){ 
-            console.log(docs, startDate, endDate);
-            resolve(docs);
-        });
-   });
-   
-}
-
 
 async function getById(id) {
-    return await User.findById(id).select('-hash');
+    const client = new Client({
+        user: "yqibwidetitjus",
+        password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+        database: "da4cq00t6pfvr5",
+        port: 5432,
+        host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+        ssl: true
+      }); 
+    
+    await client.connect()
+    const res = await client.query('select * from account where username=\'' + id + "'");
+    await client.end();
+    return res.rows[0];
 }
 
 async function create(userParam, req) {
-
-
-    // validate
-    if (await User.findOne({ username: userParam.username })) {
-        throw 'Username "' + userParam.username + '" is already taken';
-    }
-
-    const user = new User(userParam);
-
-
-    // hash password
-    if (userParam.password) {
-        user.hash = bcrypt.hashSync(userParam.password, 10);
-    }
-
-    user.isadmin = userParam.isadmin;
-
-    // save user
-    await user.save();
+    const client = new Client({
+        user: "yqibwidetitjus",
+        password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+        database: "da4cq00t6pfvr5",
+        port: 5432,
+        host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+        ssl: true
+      }); 
+    
+    await client.connect()
+    const res = await client.query('INSERT INTO account(username, password, name, isadmin)'+
+    "VALUES ('" + userParam.username + "', '" + userParam.password+ "', '" + userParam.name +"', '" + userParam.isadmin + "')");
+    await client.end();
+    return res.rows;
 }
 
 async function update(id, userParam, req) {
-
-        
-
-    const user = await User.findOne({ 'username': id });
-
- 
-
-    // validate
-    if (!user) throw 'User not found';
-    if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
-        throw 'Username "' + userParam.username + '" is already taken';
-    }
-
-    // hash password if it was entered
-    if (userParam.password) {
-        userParam.hash = bcrypt.hashSync(userParam.password, 10);
-    }
-
-
-    user.isadmin = userParam.isadmin;
-    // copy userParam properties to user
-    Object.assign(user, userParam);
-
-    user.isadmin = userParam.isadmin;
-    await user.save();
+    const client = new Client({
+        user: "yqibwidetitjus",
+        password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+        database: "da4cq00t6pfvr5",
+        port: 5432,
+        host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+        ssl: true
+      }); 
+    
+    await client.connect()
+    const res = await client.query("UPDATE account set name='" + userParam.name + 
+    "', password='" + userParam.password + "', isadmin='" + userParam.isadmin
+     +"' WHERE username='"  + userParam.username  + "'");
+    await client.end();
+    return res.rows;
 }
 
 async function _delete(id, req) {
-    let cuuruser  = await getById(req.user.sub);
-    if(!cuuruser.isadmin)
-        return;
-        
-    await User.findByIdAndRemove(id);
-}
-
-async function addImage(img) {
-
-    const image = new db.Image();
-    console.log(img);
-    image.image = Binary(img);
-    // save user
-    await image.save();
+    const client = new Client({
+        user: "yqibwidetitjus",
+        password: "c474861a5478b64850e4ed7092379f8b923b4f3fc7513f10ee755caf2ac1be7d",
+        database: "da4cq00t6pfvr5",
+        port: 5432,
+        host: "ec2-23-22-156-110.compute-1.amazonaws.com",
+        ssl: true
+      }); 
+    
+    await client.connect()
+    const res = await client.query("DELETE FROM account WHERE username='" + id + "'");
+    await client.end();
+    return res.rows;
 }
